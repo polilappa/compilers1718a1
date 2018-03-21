ec00c352e924be5d75fe405c514928f8c104abfb
Poli Lappa <p14lapp@ionio.gr>
2018-03-21 12:25:45 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,32 +1,9 @@
-
 def getchar(words,pos):
 	""" returns char at pos of words, or None if out of bounds """
 
 	if pos<0 or pos>=len(words): return None
 
-	c = words[pos]
-	
-	if (c == '0' or c == '1') and state == 'q0' :
-		return 'HOUR_01'
-	if c == '2' and state == 'q0':
-		return 'HOUR_2'
-	if (c >= '3' and c <= '9') and state == 'q0' :
-		return 'HOUR_39'
-	if (c >= '0' and c <= '9') and state == 'q1':
-		return 'HOUR_09'
-	if (c == '.' or c == ':') and state == 'q1':
-		return 'SEPARATOR_1'
-	if (c >= '0' and c <= '3') and state == 'q2':
-		return 'HOUR_03'
-	if (c == '.' or c == ':') and state == 'q2':
-		return 'SEPARATOR_1'
-	if c == '.' or c == ':' :
-		return 'SEPARATOR'
-	if (c >= '0' and c <= '5') and state == 'q4':
-		return 'MINUTE_05'
-	if (c >= '0' and c <= '9') and state == 'q5':
-		return 'MINUTE_09'
-	
+	return words[pos]
 	
 
 def scan(text,transition_table,accept_states):
@@ -59,21 +36,16 @@ def scan(text,transition_table,accept_states):
 			
 	
 # the transition table, as a dictionary
-
-# Modified
-td = { 'q0':{ 'HOUR_01':'q1', 'HOUR_2':'q2', 'HOUR_39':'q3' },
-       'q1':{ 'HOUR_09':'q3', 'SEPARATOR_1':'q4' },
-       'q2':{ 'HOUR_03':'q3', 'SEPARATOR_1':'q4' },
-       'q3':{ 'SEPARATOR':'q4' },
-       'q4':{ 'MINUTE_05':'q5' },
-       'q5':{ 'MINUTE_09':'q6' }
+td = { 'q0':{ '0':'q1','1':'q1','2':'q2','3':'q3','4':'q3','5':'q3','6':'q3','7':'q3','8':'q3','9':'q3' },
+       'q1':{ '0':'q3','1':'q3','2':'q3','3':'q3','4':'q3','5':'q3','6':'q3','7':'q3','8':'q3','9':'q3',':':'q4','.':'q4', },
+       'q2':{ '0':'q3','1':'q3','2':'q3','3':'q3','4':'q3',':':'q4','.':'q4'},
+       'q3':{ ':':'q4','.':'q4' },
+       'q4':{ '0':'q5','1':'q5','2':'q5','3':'q5','4':'q5','5':'q5' },
+       'q5':{ '0':'q6','1':'q6','2':'q6','3':'q6','4':'q6','5':'q6','6':'q6','7':'q6','8':'q6','9':'q6' }
      } 
-
 
 # the dictionary of accepting states and their
 # corresponding token
-
-# Modified
 ad = { 'q6':'TIME_TOKEN' }
 
 
